60bb190959fa4a097e9e5a60177178975458931c
MAKOTHA-JR <jumamakotha111@gmail.com>
2025-06-30 10:37:08 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "malvin~lndlBahL#0nH2atgdoZJlZ-52TWg-D5ODHUY_4KHn1V1sdBMPE64",
+SESSION_ID: process.env.SESSION_ID || "malvin~Nv9RUTIC#pJnRgoB1m7Kbij2dFLn5beW4LxNtu0tR_Qmpsp9n0aE",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
